495f558a8bd5c05d7662451ed1ba09a7cc30d551
Jarl-Penguin <jarlpenguin@outlook.com>
2021-03-20 11:36:34 +0300

    sdm632-common: Retrofit dynamic partitions
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I0cfde771f178011f8151879af041952b8e582109

--- device.mk
+++ device.mk
@@ -39,12 +39,19 @@ PRODUCT_PACKAGES += \
 
 # A/B updater
 AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_product=true \
+    POSTINSTALL_PATH_product=bin/check_dynamic_partitions \
+    FILESYSTEM_TYPE_product=ext4 \
+    POSTINSTALL_OPTIONAL_product=false
+
+AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
+    check_dynamic_partitions \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
@@ -165,6 +172,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
@@ -242,6 +253,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.target.rc \
     fstab.qcom \
+    fstab_ramdisk.qcom \
     ueventd.qcom.rc
 #    init.audio.specified_para.sh	
 
