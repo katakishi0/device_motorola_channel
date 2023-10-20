09ba4f89363a95777474b0cb435463ffba03bf4f
L1ghtzin <lightzin021@gmail.com>
2023-10-20 14:02:46 -0300

    channel: Update Perf Blobs from LA.UM.10.6.2.r1-02500-89xx.0
    Change-Id: If32b89f8d0e43019c3ba25a206c758fe83d6c86d

--- device.mk
+++ device.mk
@@ -373,8 +373,11 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power@1.2.vendor
+
+# Perf
+PRODUCT_PACKAGES+= \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # QMI
 PRODUCT_PACKAGES += \
