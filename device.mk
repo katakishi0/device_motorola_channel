e51ac19333ccb1370836f64e1ac61a7f7b045714
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    channel: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@
 
 $(call inherit-product, vendor/motorola/channel/channel-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
