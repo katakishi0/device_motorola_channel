b6c654b1452d4e144f1daeefc35895bce7bac833
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    channel: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,6 +234,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := \
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 TARGET_FS_CONFIG_GEN += \
