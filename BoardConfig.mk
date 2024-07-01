e8114e229f52d5ec303ffa7616c7d7c0ef3f74d0
Michael Bestas <mikeioannina@gmail.com>
2024-07-01 11:50:02 -0300

    channel: Include lineage FCM Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,7 +233,8 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := \
  	$(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 TARGET_FS_CONFIG_GEN += \
     $(DEVICE_PATH)/config.fs \
